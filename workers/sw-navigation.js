a66813f1506c0b499b53a76cbe6ac4527fa09ef5
David Burke <474346+bettlebrox@users.noreply.github.com>
2024-03-14 15:52:43 +0000

    openid token generation working

--- workers/sw-navigation.js
+++ workers/sw-navigation.js
@@ -11,50 +11,69 @@ import {
     ListObjectsCommand,
     DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
+var manifest = chrome.runtime.getManifest();
+var clientId = encodeURIComponent(manifest.oauth2.client_id);
+var scopes = encodeURIComponent(manifest.oauth2.scopes.join(' '));
+var redirectUri = encodeURIComponent('https://' + chrome.runtime.id + '.chromiumapp.org');
+var url = 'https://accounts.google.com/o/oauth2/auth' +
+    '?client_id=' + clientId +
+    '&response_type=id_token' +
+    '&access_type=offline' +
+    '&redirect_uri=' + redirectUri +
+    '&scope=' + scopes;
+chrome.identity.launchWebAuthFlow(
+    {
+        'url': url,
+        'interactive': true
+    },
+    function (redirectedTo) {
+        if (chrome.runtime.lastError) {
+            // Example: Authorization page could not be loaded.
+            console.log(chrome.runtime.lastError.message);
+        }
+        else {
+            var token = redirectedTo.split('#', 2)[1].split('&')[0].split('id_token=')[1];
+            // Example: id_token=<YOUR_BELOVED_ID_TOKEN>&authuser=0&hd=<SOME.DOMAIN.PL>&session_state=<SESSION_SATE>&prompt=<PROMPT>
+            console.log(token);
+            const ci = new CognitoIdentityClient({ region: "eu-west-1" })
+            const command = new GetIdCommand({
+                IdentityPoolId: "eu-west-1:d2933d30-6cef-4ea0-a6da-d0ff89f933f9",
+                Logins: { // LoginsMap
+                    "accounts.google.com": token,
+                }
+            })
+            ci.send(command, function (err, ouput) {
+                console.log("auth response: " + err + "data:" + output)
+            })
+        }
+    }
+);
+
+function createNavlog(webNavigationDetails, tab) {
+    var navlog = {
+        'type': 'navigation', 'title': tab.title, 'tabId': webNavigationDetails.tabId,
+        'timestamp': webNavigationDetails.timeStamp,
+        'documentId': webNavigationDetails.documentId,
+        'url': webNavigationDetails.url
+    }
+    if (webNavigationDetails.transitionType) {
+        navlog['transitionType'] = webNavigationDetails.transitionType
+    }
+    return navlog
+}
+
 chrome.webNavigation.onCompleted.addListener((details) => {
     chrome.tabs.get(details.tabId, function (tab) {
         if (!tab || details.url == 'about:blank' ||
             details.frameType == 'sub_frame') return;
-        console.log({
-            'type': 'navigation', 'title': tab.title, 'tabId': details.tabId,
-            'timestamp': details.timeStamp,
-            'documentId': details.documentId,
-            'url': details.url
-        })
+        console.log(createNavlog(details, tab))
     })
 });
 
 //log transitiontype of navigation
 chrome.webNavigation.onCommitted.addListener((details) => {
-
     chrome.tabs.get(details.tabId, function (tab) {
         if (!tab || details.transitionType == 'auto_subframe') return;
-        console.log({
-            'type': 'navigation', 'tabId': details.tabId,
-            'timestamp': details.timeStamp,
-            'transitionType': details.transitionType,
-            'documentId': details.documentId,
-            'url': details.url
-        })
-        chrome.identity. getAuthToken({ interactive: false }, function (token) {
-            if (chrome.runtime.lastError) {
-                alert(chrome.runtime.lastError.message);
-                return;
-            }
-            
-            console.log(token);
-            const ci = new CognitoIdentityClient({ region: "eu-west-1" })
-            const command = new GetIdCommand({
-                IdentityPoolId: "eu-west-1:d83ac7ac-b66b-44ff-a1d0-164c1adba6ec",
-                Logins: { // LoginsMap
-                    "accounts.google.com": token,
-                }
-            })
-            ci.send(command, function (data) {
-                console.log("auth response: " + data)
-            })
-
-        });
+        console.log(createNavlog(details, tab))
     })
-
 });
