22ce6e5dcd6f4caefd86c481b03f5d4ea31072c6
David Burke <474346+bettlebrox@users.noreply.github.com>
2024-03-20 16:55:37 +0000

    feat: post navlogs

--- content/content.js
+++ content/content.js
@@ -1,5 +1,7 @@
 (async () => {
     // Sends a message to the service worker and receives a tip in response
-    //await chrome.runtime.sendMessage({ type: 'content', title: document.title });
+    await chrome.runtime.sendMessage({ type: 'content', title: document.title, 
+    body_inner_html: JSON.stringify(document.body.innerHTML), 
+    url: window.location.href, body_text: document.body.innerText });
     console.log("loaded content script")
 })();
--- workers/sw-navigation.js
+++ workers/sw-navigation.js
@@ -11,6 +11,8 @@ import {
     ListObjectsCommand,
     DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
+const NAVLOG_REST_HOST = 'https://p5cgnlejzk.execute-api.eu-west-1.amazonaws.com/prod/api/navlogs';
+
 var manifest = chrome.runtime.getManifest();
 var clientId = encodeURIComponent(manifest.oauth2.client_id);
 var scopes = encodeURIComponent(manifest.oauth2.scopes.join(' '));
@@ -34,7 +36,6 @@ chrome.identity.launchWebAuthFlow(
         else {
             var token = redirectedTo.split('#', 2)[1].split('&')[0].split('id_token=')[1];
             // Example: id_token=<YOUR_BELOVED_ID_TOKEN>&authuser=0&hd=<SOME.DOMAIN.PL>&session_state=<SESSION_SATE>&prompt=<PROMPT>
-            console.log(token);
             const ci = new CognitoIdentityClient({ region: "eu-west-1" })
             const command = new GetIdCommand({
                 IdentityPoolId: "eu-west-1:d2933d30-6cef-4ea0-a6da-d0ff89f933f9",
@@ -51,10 +52,12 @@ chrome.identity.launchWebAuthFlow(
 
 function createNavlog(webNavigationDetails, tab) {
     var navlog = {
-        'type': 'navigation', 'title': tab.title, 'tabId': webNavigationDetails.tabId,
-        'timestamp': webNavigationDetails.timeStamp,
-        'documentId': webNavigationDetails.documentId,
-        'url': webNavigationDetails.url
+        'type': 'navigation', 'title': tab.title, 
+        'tabId': String(webNavigationDetails.tabId),
+        'timestamp': String(webNavigationDetails.timeStamp),
+        'documentId': String(webNavigationDetails.documentId),
+        'url': webNavigationDetails.url,
+        'body_inner_text': webNavigationDetails.body_inner_text
     }
     if (webNavigationDetails.transitionType) {
         navlog['transitionType'] = webNavigationDetails.transitionType
@@ -62,18 +65,50 @@ function createNavlog(webNavigationDetails, tab) {
     return navlog
 }
 
-chrome.webNavigation.onCompleted.addListener((details) => {
-    chrome.tabs.get(details.tabId, function (tab) {
-        if (!tab || details.url == 'about:blank' ||
-            details.frameType == 'sub_frame') return;
-        console.log(createNavlog(details, tab))
+function createNavlogFromContent(message, sender) {
+    var navlog = {
+        'type': 'content', 'title': message.title, 'tabId': String(sender.tab.id),
+        'timestamp': String(Date.now()),
+        'documentId': String(sender.tab.documentId),
+        'url': message.url,
+        'body_inner_html': 'body_inner_html' in message? message.body_inner_html.substring(0,5000):null,
+        'body_text': 'body_text' in message? message.body_text.substring(0,5000):null
+    }
+    return navlog;
+}
+
+
+function postNavlog(navlog) {
+    fetch(NAVLOG_REST_HOST, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(navlog)
     })
+}
+function getTabandPost(webNavigationDetails) {
+    chrome.tabs.get(webNavigationDetails.tabId, function (tab) {
+        if (!tab || (webNavigationDetails.url && webNavigationDetails.url == 'about:blank') ||
+            (webNavigationDetails.frameType && webNavigationDetails.frameType == 'sub_frame') ||
+            (webNavigationDetails.transitionType && webNavigationDetails.transitionType == 'auto_subframe')
+        ) return;
+        var navlog = createNavlog(webNavigationDetails, tab)
+        console.debug("posting navlog...:" + navlog)
+        postNavlog(navlog)
+    })
+}
+chrome.webNavigation.onCompleted.addListener((details) => {
+    getTabandPost(details)
 });
 
 //log transitiontype of navigation
 chrome.webNavigation.onCommitted.addListener((details) => {
-    chrome.tabs.get(details.tabId, function (tab) {
-        if (!tab || details.transitionType == 'auto_subframe') return;
-        console.log(createNavlog(details, tab))
-    })
+    getTabandPost(details)
 });
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+    console.log("received message: " + request + sender)
+    if (request.type && request.type == "content") {
+        var navlog = createNavlogFromContent(request, sender)
+        postNavlog(navlog)
+    }
+})
