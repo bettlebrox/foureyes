09ef7827f014a4f8be23f5321d5f40fb9dbb6b52
David Burke <474346+bettlebrox@users.noreply.github.com>
2024-08-23 13:06:16 +0100

    fix: disable html2canvas as not working with vite build

--- content/content.js
+++ content/content.js
@@ -1,13 +1,8 @@
 (async () => {
-    html2canvas(document.body,{allowTaint:true, useCORS:true, foreignObjectRendering: true}).then(function(canvas) {
-        // Export the canvas to its data URI representation
-       chrome.runtime.sendMessage({ type: 'content', title: document.title, 
-       body_inner_html: JSON.stringify(document.body.innerHTML), 
-       url: window.location.href, body_text: document.body.innerText, image: canvas.toDataURL("image/jpeg", 0.5) });
-       console.log("loaded content script")
-      });
-    // Sends a message to the service worker and receives a tip in response
-
+  chrome.runtime.sendMessage({ type: 'content', title: document.title, 
+    body_inner_html: JSON.stringify(document.body.innerHTML), 
+    url: window.location.href, body_text: document.body.innerText });
+    console.log("loaded content script");
 })();
 window.addEventListener("message", function(event) {
     // Only accept messages from the same origin
