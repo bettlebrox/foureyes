f6f0947fbfd9988c782a9e1c37fdac57e0019af6
David Burke <474346+bettlebrox@users.noreply.github.com>
2024-12-19 14:35:31 +0000

    feat: add omnibox support

--- workers/sw-navigation.tsx
+++ workers/sw-navigation.tsx
@@ -1,9 +1,42 @@
 import { Amplify } from "aws-amplify";
-import { post } from "aws-amplify/api";
+import { get, post } from "aws-amplify/api";
 import outputs from "../workers/amplify_outputs.json";
 import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
 import { ChromeStorage } from "../common/ChromeStorage";
 import { getCurrentUser } from "aws-amplify/auth";
+
+export interface DassieItem {
+  id: string;
+  title: string;
+  original_title: string;
+  summary: string | null;
+  created_at: string;
+  updated_at: string;
+  source: string;
+}
+
+export interface Theme extends DassieItem {
+  score?: number;
+  recurrent_count: number;
+  sporadic_count: number;
+  article_count: number;
+}
+
+export interface Article extends DassieItem {
+  word_count: number;
+  score?: number;
+  themes: DassieItem[];
+  logged_at: string;
+  text: string;
+  url: string;
+  image: string | null;
+}
+
+
+export interface SearchResult {
+  themes: Theme[];
+  articles: Article[];
+}
 
 Amplify.configure(outputs);
 const existingConfig = Amplify.getConfig();
@@ -123,6 +156,42 @@ async function postNavlog(navlog: NavLog) {
   }
 }
 
+async function search({ sortField,max,query }: { sortField: string,max: number,query: string }) {
+  sortField = sortField || 'count_association';
+  max = max || 10;
+  query = query || '';
+  const { body } = await get({
+    apiName: 'Dassie',
+    path: '/api/search/' + query,
+    options: {
+      queryParams: { sortField, max: max.toString(), query },
+    },
+  }).response;
+  return JSON.parse(await body.text()) as SearchResult;
+};
+
+async function postTopic(title: string, handleSubmitComplete: (response: Theme | Error) => void) {
+  try {
+    const restOperation = post({
+      apiName: 'Dassie',
+      path: '/api/themes',
+      options: {
+        body: {
+          title: title,
+        },
+      },
+    });
+    const { body } = await restOperation.response;
+    const response = (await body.json()) as unknown as Theme; // Cast to unknown first
+    handleSubmitComplete(response);
+    console.log('POST call succeeded');
+    console.log(response);
+  } catch (error) {
+    console.log('POST call failed: ');
+    handleSubmitComplete(error instanceof Error ? error : new Error('Unknown error'));
+  }
+}
+
 function getTabAndPost(
   webNavigationDetails:
     | chrome.webNavigation.WebNavigationTransitionCallbackDetails
@@ -177,4 +246,63 @@ chrome.runtime.onMessage.addListener((request, sender) => {
 chrome.runtime.onInstalled.addListener(() => {
   chrome.sidePanel.setOptions({ path: "sidepanel.html" });
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
+
+chrome.omnibox.onInputStarted.addListener(function () {
+  chrome.omnibox.setDefaultSuggestion({
+    description:
+      "Add Topic"
+  });
+});
+
+chrome.omnibox.onInputChanged.addListener(async function (text, suggest) {
+  const searchResult = await search({ sortField: 'count_association', max: 10, query: text });
+  const themes = searchResult.themes;
+  const suggestions = themes.map(theme => ({
+    content: "theme:" + theme.title,
+    description: "<match>"+theme.original_title+"</match>" + (theme.summary ? " - <dim>" + theme.summary + "</dim>" : ""),
+    deletable: false
+  }));
+  suggest(suggestions);
+  chrome.omnibox.setDefaultSuggestion({
+    description:
+      "Add Topic - <match>" + text + "</match>"
+  });
+});
+
+function handleTabs(url: string, disposition: chrome.omnibox.OnInputEnteredDisposition) {
+  if (disposition == "currentTab") {
+    chrome.tabs.update({ url: url });
+  }
+  chrome.tabs.create({ url: url, active: disposition == "newForegroundTab" });
+}
+
+chrome.omnibox.onInputEntered.addListener(function (text, disposition) {
+  console.debug("onInputEntered: " + text + " " + disposition);
+  // Encode user input for special characters , / ? : @ & = + $ #
+  let newURL = 'https://main.d1tgde1goqkt1z.amplifyapp.com/search/' + encodeURIComponent(text);
+  if (text.startsWith("theme:")) {
+    const theme = text.substring(6);
+    newURL = 'https://main.d1tgde1goqkt1z.amplifyapp.com/theme/' + theme;
+    handleTabs(newURL, disposition);
+  }else{
+    postTopic(text, (response) => {
+      if (response instanceof Error) {
+        console.error("Error posting topic: " + response.message);
+      } else {
+        console.log("Topic posted successfully: " + response.title);
+        newURL = 'https://main.d1tgde1goqkt1z.amplifyapp.com/theme/' + response.title;
+        handleTabs(newURL, disposition);
+      }
+    });
+  }
+});
+
+
+chrome.omnibox.onInputCancelled.addListener(function () {
+  console.debug("onInputCancelled");
+});
+
+chrome.omnibox.onDeleteSuggestion.addListener(function (text) {
+  console.debug("onDeleteSuggestion: " + text);
 });
